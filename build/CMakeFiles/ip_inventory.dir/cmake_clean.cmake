43243bfcb39b53d4399aaa29ba0155f634ddd0a1
gri40 <gpggogo@gmail.com>
2026-03-03 09:36:27 +0200

    Add project files.

---
+++ build/CMakeFiles/ip_inventory.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ip_inventory.dir/ip-inventory.cpp.o"
+  "CMakeFiles/ip_inventory.dir/ip-inventory.cpp.o.d"
+  "ip_inventory"
+  "ip_inventory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ip_inventory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ip-inventory.cpp
@@ -0,0 +1,12 @@
+﻿// ip-inventory.cpp : Defines the entry point for the application.
+//
+
+#include "ip-inventory.h"
+#include "thirdparty/json.hpp"
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake111." << endl;
+	return 0;
+}
